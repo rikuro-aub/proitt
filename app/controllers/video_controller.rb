1f70c9c34ad24635c4568120db6c2fad756109c7
3no3 <ulis7524@gmail.com>
2019-10-15 15:24:19 +0900

    トップページにTagごとのサムネイル表示列を作成

---
+++ app/controllers/video_controller.rb
@@ -0,0 +1,13 @@
+class VideoController < ApplicationController
+  def index
+    display_items_by_tag = 4
+    @videos = {}
+
+    # トップページ表示用の動画を取得する
+    @tags = Tag.all.order(:id)
+    @tags.each do |tag|
+      # いいね数/視聴回数 が上位の動画をトップページに表示する
+      @videos.merge!(tag.tag.to_sym => Video.where(tag_id: tag).order('like_count/view_count DESC').limit(display_items_by_tag))
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'video#index'
+  resources :videos, only: [:index, :show]
 end
