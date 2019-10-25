70638958ff9c695c64a84683bd5d12f29f7c0895
3no3 <ulis7524@gmail.com>
2019-10-25 16:37:00 +0900

    modelのUserにuser_idのcolumnを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_25_010142) do
+ActiveRecord::Schema.define(version: 2019_10_25_072619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,8 @@ ActiveRecord::Schema.define(version: 2019_10_25_010142) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "token_digest"
+    t.string "token_digest", null: false
+    t.string "user_id", null: false
   end
 
   create_table "videos", force: :cascade do |t|
